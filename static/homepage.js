f0589e01bdd5434fee20bd21b66199dd4728fb48
Michael Linetsky <linetsky@gmail.com>
2013-10-30 17:13:18 -0700

    further work on front page log procedure

--- static/homepage.js
+++ static/homepage.js
@@ -10,9 +10,28 @@ appTemplates.loginfirst = hereDoc(function(){/*
 
 appTemplates.homepage = hereDoc(function(){/*
 	<div class="row">
-		<form id="logprocedure "class="col-sm-6">
+		<form id="logprocedure" role="form" class="col-sm-3 col-sm-offset-2">
+		<div class="form-group">
 			<h3>Log a Procedure</h3>
-			<label for=></label>
+			<label for="acc" class="control-label sr-only">Procedure accession number</label>
+			<input id="acc" type="text" placeholder="Accession Number" class="form-control" />
+			<label for="procedure type" class="control-label sr-only">Procedure type</label>
+			<select id="procedure type" class="form-control">
+				<option value="lumbar puncture">Lumbar Puncture</option>
+				<option value="intrathecal chemo">Intrathecal Chemotherapy</option>
+				<option value="lumbar drain">Lumbar drain placement</option>
+				<option value="ct biopsy">CT-guided biopsy</option>
+				<option value="us biopsy">Ultrasound-guided biopsy</option>
+				<option value="epidural injection">Epidural injection</option>
+				<option value="facet injection">Facet injection</option>
+				<option value="cerebral angiogram">Cerebral angiogram</option>
+				<option value="spinal angiogram">Spinal angiogram</option>
+				<option value="other">Other</option>
+			</select>
+			<label for="date" class="control-label sr-only">Date of procedure</label>
+			<input type="date" class="form-control" id="date" />
+		</div>
+		<button type="submit" id="submit_log" class="btn btn-success">Log Procedure</button>
 		</form>
 		<form id="entercase" class="col-sm-6">
 			<h3>Enter a Case</h3>
