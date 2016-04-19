ef315d044cee2600f758646ec863c0d447b01e9b
Michael <milinets@users.noreply.github.com>
2016-04-19 11:11:26 -0700

    basic db functions

--- dbfuncs.py
+++ dbfuncs.py
@@ -5,37 +5,49 @@ import uuid
 import json
 import os
 import socket
-import platform
-
-thishost = platform.uname()
-
-pg_conn_string = """
-                    dbname='postgres' 
-                    user='postgres'
-    """
 
 def connect_db():
     return sq.connect('example.db')
-    
+
+def createTable(name):
+    con = connect_db()
+    with con:
+        cur = con.cursor()
+        cur.execute("""
+            CREATE TABLE {}
+                (id TEXT PRIMART KEY,
+                type TEXT,
+                info TEXT,
+                timecreated TEXT,
+                createdby TEXT,
+                log TEXT)
+        """.format(name))
+
+def show_rows(name):
+    conn = connect_db()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT *
+            FROM {}
+        """.format(name))
+        for i in cur.fetchall():
+            print i
 
 
 def query_db(query, args=(), one=False):
     con = connect_db()
-    cur = con.cursor()
-    cur.execute(query, args)
-    con.commit()
-    con.close()
-
-def createdb(name):
-    conn = connect_db()
-    cur = conn.cursor()
-#    cur.execute("CREATE EXTENSION uuid-ossp")
-#    cur.execute("CREATE TABLE db (id uuid primary key default uuid_generate_v4(), data json)")
-    cur.execute("CREATE TABLE db (id uuid primary key, data json)", (name,))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with con:
+        cur = con.cursor()
+        cur.execute(query, args)
 
 
+import datetime as dt 
+import base64
+import random
 
-
+def gen_id():
+    return base64.urlsafe_b64encode(str(random.random()))[-10:]
+                
+for i in range(10):
+    print gen_id()
